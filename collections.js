b13721c20c006a6b3e7d415ffa279d4925ee4205
RAMASAMYABISHAKE <ramasamyabishake506@gmail.com>
2024-01-29 23:53:46 +0800

    updated nostra project

---
+++ collections.js
@@ -0,0 +1,37 @@
+// slecting top
+var top = document.getElementById("top")
+var topx = document.querySelector(".top__para")
+// 
+function gone() {
+    document.getElementById("top").style.display = "none"
+}
+
+//selecting side nav bar
+var sidenav = document.getElementById("sidenav")
+var menu = document.getElementById("menu")
+var sidetext = document.getElementById("side-text")
+//
+
+
+function menuicon() {
+    document.getElementById("sidenav").style.left = 0
+}
+function sidego() {
+    document.getElementById("sidenav").style.left = "-60%"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
